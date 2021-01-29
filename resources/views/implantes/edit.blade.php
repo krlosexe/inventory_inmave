d43c33595cba8b0347340290f78406b2f990ae52
Gil <dev.javiergil@gmail.com>
2021-01-29 17:46:14 -0500

    ajuste

--- resources/views/implantes/edit.blade.php
+++ resources/views/implantes/edit.blade.php
@@ -74,8 +74,8 @@
                                     <th>Serial</th>
                                     <th>Lote</th>
                                     <th>Registro INVIMA</th>
-                                    <th>Vence</th>
-                                    <th>Valor</th>
+                                    <th>F.Vencimiento</th>
+                                    <!-- <th>Valor</th> -->
                                     <th>Descripción</th>
                                     <th>Gramaje</th>
                                     <th>perfil</th>
