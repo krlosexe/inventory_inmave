6a35fcfbcbb9cd89c5724fecf79d6c8a9e29c542
Gil <dev.javiergil@gmail.com>
2021-02-22 09:13:42 -0500

    ajuste

--- resources/views/warehouse/output/edit.blade.php
+++ resources/views/warehouse/output/edit.blade.php
@@ -72,10 +72,10 @@
                             <tfoot>
                                 <tr>
                                     <th style="text-align: center;" id="descuento_text">
-                                        <label for="apply_discount">¿ Quieres Aplicar Descuento (10%)?</label>
+                                        <label for="apply_discount_edit">¿ Quieres Aplicar Descuento (10%)?</label>
                                         <input type="checkbox" id="apply_discount_edit" class="form-control">
                                         <br><br>
-                                        <label for="apply_discount2">¿ Quieres Aplicar Descuento (15%)?</label>
+                                        <label for="apply_discount_edit2">¿ Quieres Aplicar Descuento (15%)?</label>
                                         <!-- <input style="text-align: right;" type="text" class="form-control monto_formato_decimales discount" name="discount" id="discount" value="0">-->
                                         <input type="checkbox" id="apply_discount_edit2" class="form-control">
                                     </th>
